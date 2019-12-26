04adf3d2cd4930dc2dd750ae0ff7b715ae39a704
AlMuz <almuz@inbox.lv>
2019-12-26 20:08:20 +0200

    Users page with dynamic specific user route

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -15,6 +15,11 @@
             About
           </nuxt-link>
         </li>
+        <li class="nav-item">
+          <nuxt-link active-class="active" class="nav-link" to="/users">
+            Users
+          </nuxt-link>
+        </li>
       </ul>
     </div>
   </nav>
