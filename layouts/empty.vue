4b35430969a0237e7af6007f78df21f63e06c4a0
AlMuz <almuz@inbox.lv>
2019-12-26 20:35:10 +0200

    Login page and empty layout for it

---
+++ layouts/empty.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="empty-layout">
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+.empty-layout {
+  display: flex;
+  justify-content: center;
+  padding-top: 50px;
+}
+</style>
