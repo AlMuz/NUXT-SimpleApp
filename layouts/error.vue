e13ebbaeb20443cc8a4da6968770cbb632d78bf8
AlMuz <almuz@inbox.lv>
2019-12-26 20:12:40 +0200

    404 page added

---
+++ layouts/error.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <h1>Erorr 404</h1>
+    <nuxt-link to="/">
+      Back home
+    </nuxt-link>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+section {
+  padding-top: 50px;
+}
+h1 {
+  color: red;
+}
+</style>
